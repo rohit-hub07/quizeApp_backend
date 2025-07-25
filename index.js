9d5193298596fa10c48d2deae2f823e3996caeaa
rohitsingh <rohitsingh986491@gmail.com>
2025-07-26 00:26:16 +0530

    added cors headers

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
