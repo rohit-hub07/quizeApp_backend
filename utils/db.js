a8b0cd0bfebb89082a76c1226536408bc0f8e5e8
rohitsingh <rohitsingh986491@gmail.com>
2025-07-25 23:21:34 +0530

    deployment issue

--- utils/db.js
+++ utils/db.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// "mongodb://127.0.0.1:27017/quizeApp"
 export const db = async () => {
   const mongoUri =
-    process.env.MONGO_URI || "mongodb://127.0.0.1:27017/quizeApp";
+    process.env.MONGO_URI;
 
   mongoose
     .connect(mongoUri)
